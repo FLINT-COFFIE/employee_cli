5e748f0052d1de4c53e86ef64276762c2709eff0
Flint Coffie <coffieflint91@gmail.com>
2026-08-17 14:51:02 +0000

    added inputs and add employee function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # import employee class
 from employee import Employee
 
+# store employees
+employees = {}
+
+
+# functions
+def add_employee():
+    first = input("Enter first name: ")
+    last = input("Enter last name: ")
+    pay = input("Enter pay amount: ")
+    index = first
+    first = Employee(first, last, pay)
+    employees[index] = first
+    return employees
+
 
 def main():
     # Visuals
@@ -8,8 +22,22 @@ def main():
     print("===  Employee Manager  ===")
     print("==========================")
 
-    #
+    # Inputs
+    print("1. Add Employee\n")
+    print("2. Erase Employee\n")
+    print("3. Employee details\n")
+    print("4. Total number of Employees\n")
+
+    # adding employees
+    while True:
+        user_input = input("Enter your input: ")
+        if not user_input in range(1:5):
+            raise ValueError ("Incorrect Input")
+    
+        if user_input == 1:
+            add_employee()
 
 
 if __name__ == "__main__":
     main()
+    # print(employees)
